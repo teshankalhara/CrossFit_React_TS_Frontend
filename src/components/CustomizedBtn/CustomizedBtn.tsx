10129c05c28503a9307835576ce39bc1f3ca8502
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-10-06 22:52:55 +0530

    customizedBtn

--- src/components/CustomizedBtn/CustomizedBtn.tsx
+++ src/components/CustomizedBtn/CustomizedBtn.tsx
@@ -1,13 +1,14 @@
 import CustomizedBtnType from "../../types/CustomizedBtnType"
+import arrowIcon from "../../assets/img/arrowIcon.png"
 
 const CustomizedBtn = (btn: CustomizedBtnType) => {
     return (
         <>
             <button type="button" style={{ width: `${btn.width}px`, height: `${btn.height}px` }}
-                className="border  hover:border-white text-white hover:text-black hover:bg-slate-500 py-2 rounded-3xl flex justify-between items-center">
+                className="border  hover:border-white text-white hover:bg-slate-500 py-2 rounded-3xl flex justify-between items-center">
                 <div className="px-8 text-medium text-left">{btn.title}</div>
-                <div className="border hover:border-black rounded-full px-1 py-2 text-sm" style={{ width: `${btn.height}px`, height: `${btn.height}px` }}>
-                    Icon
+                <div className="rounded-full" style={{ width: `${btn.height}px`, height: `${btn.height}px` }}>
+                    <img src={arrowIcon} alt="Arrow Icon" width={btn.height} />
                 </div>
             </button>
         </>

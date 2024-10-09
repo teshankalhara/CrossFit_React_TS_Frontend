a78bde6fe486e447338611d42f7581b47fbadbad
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-10-10 02:50:20 +0530

    bmi page

--- src/components/InputBMI/InputBMI.tsx
+++ src/components/InputBMI/InputBMI.tsx
@@ -6,19 +6,19 @@ const InputBMI = () => {
             <form className="py-4">
                 <div className="flex">
                     <div>
-                        <input type="text" style={{ backgroundColor: "#131619" }} className="w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white" placeholder="Height/cm" />
+                        <input type="text" style={{ backgroundColor: "#131619" }} className="w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Height/cm" />
                     </div>
                     <div>
-                        <input type="text" style={{ backgroundColor: "#131619" }} className="ml-1 w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white" placeholder="Weight/kg" />
+                        <input type="text" style={{ backgroundColor: "#131619" }} className="ml-1 w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Weight/kg" />
                     </div>
                 </div>
                 <div className="flex mt-2">
                     <div>
-                        <input type="text" style={{ backgroundColor: "#131619" }} className="w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white" placeholder="Height/cm" />
+                        <input type="text" style={{ backgroundColor: "#131619" }} className="w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Height/cm" />
                     </div>
                     <div>
-                        <select style={{ backgroundColor: "#131619" }} className="ml-1 w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 text-white">
-                            <option value="" disabled selected>Gender</option>
+                        <select style={{ backgroundColor: "#131619" }} className="text-xs ml-1 w-[200px] h-[40px] border border-slate-300 rounded-full pl-5 p-2 text-white">
+                            <option value="" disabled selected >Gender</option>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                             <option value="other">Other</option>
@@ -27,7 +27,7 @@ const InputBMI = () => {
                 </div>
                 <div className="flex mt-2">
                     <div>
-                        <input type="text" style={{ backgroundColor: "#131619" }} className="w-[404px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white" placeholder="Select an activity factor :" />
+                        <input type="text" style={{ backgroundColor: "#131619" }} className="w-[404px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Select an activity factor :" />
                     </div>
                 </div>
                 <div className='flex mt-2'>

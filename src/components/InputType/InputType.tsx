f3b0355bf42577c22f4ecca64102956fb02ebb3c
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-10-09 23:12:09 +0530

    inputType box

--- src/components/InputType/InputType.tsx
+++ src/components/InputType/InputType.tsx
@@ -16,8 +16,13 @@ const InputType = () => {
                         <input type="text" className=" bg-black w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white" placeholder="Height/cm" />
                     </div>
                     <div>
-                        <input type="text" className="ml-2 bg-black w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white" placeholder="Gender" />
-                    </div>
+    <select className="ml-2 bg-black w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 text-white">
+        <option value="" disabled selected>Gender</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
+    </select>
+</div>
                 </div>
                 <div className="flex mt-2">
                     <div>

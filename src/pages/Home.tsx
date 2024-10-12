e5c7c87aee43096c232d4e1556a9d3a29eaef16d
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-10-12 16:26:44 +0530

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
 const Home = () => {
     return (
         <>
-            <div className="pt-4">
-                <div className="text-white">
-                    Home
+            <div className="pt-4 relative pb-10" style={{ backgroundColor: "#131619" }}>
+                <div className="pt-28">
+                    <div className="text-white uppercase mb-0 absolute pl-56">
+                        <div className="pl-16 m-0">new event</div>
+                        <div className="pl-16 m-0">coming up / december 7 - 21</div>
+                    </div>
+                    <div className="text-white text-center mt-0">
+                        <h1 className="uppercase font-bold" style={{ fontSize: "150px" }}>crossfit</h1>
+                    </div>
                 </div>
             </div>
         </>

0d7cd5c32201e2ba05f3e1b6a5f82c3933a2e3e8
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-10-13 23:08:07 +0530

    responsive-checkbmi_page

--- src/components/InputBMI/InputBMI.tsx
+++ src/components/InputBMI/InputBMI.tsx
@@ -3,23 +3,23 @@ import CustomizedBtn from '../CustomizedBtn/CustomizedBtn'
 const InputBMI = () => {
     return (
         <>
-            <form className="py-4 pt-5">
-                <div className="flex">
+            <form className="sm:py-4 sm:pt-5 py-5 pb-8 mb-2">
+                <div className="sm:flex">
                     <div>
-                        <input type="text" style={{ backgroundColor: "#131619" }} className="w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Height/cm" />
+                        <input type="text" style={{ backgroundColor: "#131619" }} className="m-2 sm:m-0 w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Height/cm" />
                     </div>
                     <div>
-                        <input type="text" style={{ backgroundColor: "#131619" }} className="ml-1 w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Weight/kg" />
+                        <input type="text" style={{ backgroundColor: "#131619" }} className="m-2 sm:m-0 sm:ml-1 w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Weight/kg" />
                     </div>
                 </div>
-                <div className="flex mt-2">
+                <div className="sm:flex sm:mt-2">
                     <div>
-                        <input type="text" style={{ backgroundColor: "#131619" }} className="w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Height/cm" />
+                        <input type="text" style={{ backgroundColor: "#131619" }} className="m-2 sm:m-0 w-[200px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Height/cm" />
                     </div>
                     <div>
                         <select
                             style={{ backgroundColor: "#131619" }}
-                            className="text-xs ml-1 w-[200px] h-[40px] border border-slate-300 rounded-full pl-5 p-2 text-white"
+                            className="m-2 sm:m-0 sm:ml-1 text-xs w-[200px] h-[40px] border border-slate-300 rounded-full pl-5 p-2 text-white"
                             defaultValue=""
                         >
                             <option value="" disabled>
@@ -32,12 +32,12 @@ const InputBMI = () => {
                     </div>
 
                 </div>
-                <div className="flex mt-2">
+                <div className="sm:flex sm:mt-2 m-2 sm:-m-0">
                     <div>
-                        <input type="text" style={{ backgroundColor: "#131619" }} className="w-[404px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Select an activity factor :" />
+                        <input type="text" style={{ backgroundColor: "#131619" }} className="sm:m-0 w-[340px] sm:w-[404px] h-[40px] border text-sm border-slate-300 rounded-full pl-5 p-2 placeholder:text-white placeholder:text-xs" placeholder="Select an activity factor :" />
                     </div>
                 </div>
-                <div className='flex mt-4'>
+                <div className='sm:flex sm:pr-16 m-2 sm:-m-0 sm:mt-4 sm:justify-start'>
                     <div>
                         <CustomizedBtn title='CALCULATE' width={200} height={40} />
                     </div>
--- src/pages/BMI.tsx
+++ src/pages/BMI.tsx
@@ -5,24 +5,24 @@ const BMI = () => {
     return (
         <>
             <div className="pt-4 py-5" style={{ backgroundColor: "#131619" }}>
-                <div className="px-8 text-slate-100">
-                    <div className="flex">
-                        <div className="relative basis-1/2 px-10 py-2">
-                            <h1 className="absolute inset-0 text-[40px] sm:text-[50px] md:text-[70px] lg:text-[80px] font-extrabold text-black opacity-10 flex items-start justify-start"
+                <div className="lg:px-8 text-slate-100">
+                    <div className="lg:flex">
+                        <div className="relative lg:basis-1/2 lg:py-2 sm:px-10 sm:pt-10 pl-5 pt-2">
+                            <h1 className="absolute lg:inset-0 text-[80px] sm:text-[70px] lg:text-[80px] font-extrabold text-black opacity-10 sm:flex sm:items-start sm:justify-start"
                                 style={{ textShadow: '2px 2px 0px white, -2px -2px 0px white, 2px -2px 0px white, -2px 2px 0px white' }}>
                                 BMI
                             </h1>
-                            <div className="relative z-10 py-3 mt-4">
-                                <h1 className="mb-1 text-slate-50 font-bold" style={{ fontSize: "42px" }}>INPUT YOUR BMI</h1>
+                            <div className="lg:relative lg:z-10 lg:py-3 lg:mt-4 sm:mt-6 mt-8">
+                                <h1 className="lg:mb-1 text-slate-50 font-bold" style={{ fontSize: "42px" }}>INPUT YOUR BMI</h1>
                             </div>
-                            <div className="px-6 relative z-99">
+                            <div className="lg:px-6 lg:relative lg:z-99">
                                 <p>
                                     Body Mass Index (BMI) is a measure that uses weight and height to determine if a person is underweight, normal, overweight, or obese, indicating health risks.
                                 </p>
                                 <InputBMI/>
                             </div>
                         </div>
-                        <div className="basis-1/2 pt-8">
+                        <div className="lg:basis-1/2 lg:pt-8">
                             <div className="flex justify-center items-center">
                                 <BMITable />
                             </div>
